5b00dbc5df9e869fc85df71aa4407a76826d18f4
Abner_Lucas <abnerlucas076@gmail.com>
2024-09-19 20:24:30 -0300

    Adicionando o Article na pagina
    Coloquei todos os Articles novamente com acessibilidade atualizada indicando todas as Divs e todos os links.

---
+++ atividade_04/index.php
@@ -0,0 +1,64 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/topo.css">
+
+</head>
+<body>
+
+    <?php require "header.inc.php"; ?>
+
+    <div class="container">
+        <main>
+            <div class="row justify-content-md-center">
+                <article class="col-12">
+                    <h2 hidden> Pacatos Pinguins Piratas do Deserto Proibido </h2>
+                    <figure class="centropagina">
+                        <img class="img" src="imagem/header.webp" alt="é uma imagem de apresentação com o nome do grupo (Pacatos pinguins piratas do deserto proibido) em negrito com o fundo roxo">
+                    </figure>
+                    <p> Seja bem-vindo ao lugar onde a cultura nerd ganha vida. Descubra as últimas novidades, encontre eventos imperdíveis e junte-se à comunidade dos apaixonados por games, quadrinhos, filmes, séries e muito mais. Nos falamos de eventos de cultura nerd, como objetivo principal sendo proporcionar uma plataforma dinâmica e inclusiva para os apaixonados por esse universo tão fascinante. Aqui, buscamos conectar fãs de games, quadrinhos, filmes, séries, cosplay e tudo mais que envolve a cultura geek, oferecendo um conteudo sobre uma variedade de eventos relacionados a esse universo. </p>
+                </article> 
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-4 margem8" role="article">
+                    <section class="corroxa">
+                        <figure class="centropagina">
+                            <img class = "img img2" src="imagem/dn2.webp" alt="é uma da logo do domingo nerd, com cada um dos mascotes que teve em cada ano">
+                        </figure>
+                        <h4 class="centropagina margemtop2"> Domingo Nerd</h4>
+                        <p class="margemnoticias"> Prepare-se para mergulhar no universo geek no Domingo Nerd! Com uma mistura de cultura pop, jogos, quadrinhos e muito mais, em um ambiente acolher a todos, que reúne entusiastas de todas as vertentes nerds para compartilhar sua paixão com outros aficionados. </p>
+                        <a href="https://www.einerd.com.br/netflix-aumenta-precos/" class="btn btn-primary margemnoticias" aria-label="Ir para o site do Domingo Nerd">Ver mais</a>
+                    </section>
+                </div>
+            </div> 
+            <div class="col-sm-12 col-md-12 col-lg-4 margem8" role="article">
+                <section class="corroxa">
+                    <figure class="centropagina">
+                        <img class = "img img2" src="imagem/fatecon.webp" alt="é uma da logo da Fatecon, com seu mascote com uma mochila a jato, descrito do lado Fatecon Hqs">
+                    </figure>
+                    <h4 class="centropagina margemtop2"> Fatecon </h4>
+                    <p class="margemnoticias"> Explore um evento que oferece uma atmosfera envolvente e estimulante, onde fãs de quadrinhos e literatura exploram lançamentos de livros, quadrinhos e outras obras, tendo a chance de conhecerem pessoalmente e interagirem diretamente com os criadores por trás dessas obras. </p>
+                    <a href="https://www.einerd.com.br/netflix-aumenta-precos/" class="btn btn-primary margemnoticias" aria-label="Ir para o site da Fatecon">Ver mais</a>
+                </section>
+            </div> 
+            <div class="col-sm-12 col-md-12 col-lg-4 margem8" role="article">
+                <section class="corroxa">
+                    <figure class="centropagina">
+                        <img class = "img img2" src="imagem/ccxp.webp" alt="é uma da logo da CCXP, com o nome CCXP descrito">
+                    </figure>
+                    <h4 class="centropagina margemtop2"> CCXP </h4>
+                    <p class="margemnoticias"> A CCXP abre suas portas para uma experiência única. Este é o lugar onde a cultura pop ganha vida, onde fãs de todas as idades se reúnem para celebrar o que há de mais incrível nos quadrinhos, cinema, TV, games que em cada esquina há novas aventuras e estandes que guardam segredos à serem revelados. </p>
+                    <a href="https://www.einerd.com.br/netflix-aumenta-precos/" class="btn btn-primary margemnoticias" aria-label="Ir para o site da CCXP">Ver mais</a>
+                </section>
+            </div>  
+
+    
+        </main>
+
+    </div>
+</body>
+</html>
