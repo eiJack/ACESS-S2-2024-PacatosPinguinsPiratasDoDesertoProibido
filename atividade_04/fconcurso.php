530e14d09dfc6675eb04350fed9597b8d7fe7ca5
Mateus Oliveira <179388557+mateusol9812@users.noreply.github.com>
2024-10-02 12:30:01 -0300

    [Trabalho 4] Reformulação das páginas Notícias, Artistas da Fatecon e Concursos da Fatecon. Foi realizado a inserção de tags para melhorar a semântica do html e editado e adicionado recursos de acessibilidade do wai-aria para melhorar a acessibilidade.

---
+++ atividade_04/fconcurso.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Concursos FATECon</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/dn.estilo.css">
+    <link rel="stylesheet" href="css/concursos.css">
+    <link rel="stylesheet" href="css/topo.css">
+</head>
+<body>
+
+<?php require "header.inc.php"; ?>
+<?php require "dn.nav.inc.fatecon.php"; ?>
+
+<main class="mainprincipal bordas">
+            <header>
+            <h1 class="hconcursos text-center"> <i class="bi bi-award"></i> Concursos</h1>
+            <p class="text-center">Segue abaixo concursos realizados pela FATECon!</p>
+            </header>
+            <div class="row col-12">
+
+                <section class="col-sm-12 col-md-6 col-lg-6 shadow p-3 mb-5 rounded sectionconcursos">
+                    <div class="borda">
+                    <h2 class="h2concursos">Concurso Desenhe São Roque 2023</h2>
+                    
+                        <div class="row">
+                        <figure class="figure col-12 imgleft figuremargin">
+                            <h3 class="textoimg">Anúncio do Concurso Desenhe São Roque</h3>              
+                            <a href="#"><img class="figure-img img-fluid imgcem" src="imagem/fateconconcdesesr/fateconc1figura1.webp" alt="Anúncio do Concurso Desenhe São Roque, o anúncio possui o desenho do mascote da Fatecon junto do nome da Fatecon desenhado no estilo grafite e mais um texto onde está escrito concurso desenhe São Roque."></a>
+                            <figcaption class="figure-caption text-right">Fonte: Página FATECon Facebook.</figcaption>
+                        </figure>
+                        </div>
+                        <p>O concurso desenhe São Roque foi um concurso realizado pela FATECon onde os artistas
+                             deviam escolher entre as categorias São Roque Antiga, Atual ou do Futuro. Os artistas
+                              selecionados tiveram suas obras exibidas na FATECon 2023 e o publico contribuiu com votos
+                               para seleção das 3 melhores obras.
+                        </p>
+                        <div class="row col-12">
+
+                        <figure class="figure col-12 imgleft figuremargin">
+                        <h3 class="textoimg">Primeira Colocada do Concurso</h3>              
+                            <a href="imagem/fateconconcdesesr/fateconc1figura2.webp"><img class="figure-img img-fluid imgcem" src="imagem/fateconconcdesesr/fateconc1figura2.webp" alt="Arte do Primeiro Colocado do Concurso, na arte é possível observar uma garota de cabelo roxo ao lado de um prédio em amarelo onde está escrito Vinícola Góes. Logo abaixo está o nome da artista: Emilly Yukari Bastos Maruyama, 16 Anos."></a>
+                            <figcaption class="figure-caption text-right">Fonte: Página FATECon Facebook.</figcaption>
+                        </figure>
+
+                        <figure class="figure col-6 imgleft figuremargin">
+                        <h3 class="textoimg">Segunda Colocada do Concurso</h3>               
+                            <a href="imagem/fateconconcdesesr/fateconc1figura3.webp"><img class="figure-img img-fluid imgcem" src="imagem/fateconconcdesesr/fateconc1figura3.webp" alt="Arte do Segundo Colocado do Concurso, na arte é possível ver a imagem de uma igreja ao anoitecer e logo a volta várias casas. Logo abaixo está o nome da artista: Nathalia Garcia de Almeida, 15 Anos."></a>
+                            <figcaption class="figure-caption text-right">Fonte: Página FATECon Facebook.</figcaption>
+                        </figure>
+
+                        <figure class="figure col-6 imgleft figuremargin">  
+                        <h3 class="textoimg">Terceiro Colocado do Concurso</h3>             
+                            <a href="imagem/fateconconcdesesr/fateconc1figura4.webp"><img class="figure-img img-fluid imgcem" src="imagem/fateconconcdesesr/fateconc1figura4.webp" alt="Arte do Terceiro Colocado do Concurso, nesta arte é possível observar uma cidade com duas vias de carro e vários prédios em volta. Logo abaixo está o nome do artista: Miguel de Oliveira Assêncio Dutra ,17 Anos."></a>
+                            <figcaption class="figure-caption text-right">Fonte: Página FATECon Facebook.</figcaption>
+                        </figure>
+
+                        </div>
+                        
+                        </div>
+                </section>
+
+
+                <section class="col-sm-12 col-md-6 col-lg-6 shadow p-3 mb-5 rounded sectionconcursos">
+                <div class="borda">
+                    <h2 class="h2concursos">Desfile Cosplay da FATECon 2023</h2>
+                        <div class="row">
+                        <figure class="figure col-12 imgleft figuremargin">
+                        <h3 class="textoimg">Anúncio Desfile Cosplay FATECon</h3>               
+                            <a href="#"><img class="figure-img img-fluid imgcem" src="imagem/fateconconccosplay/fateconc2figura1.webp" alt="Anúncio Desfile Cosplay FATECon, na imagem do anúncio é possível ver uma garota com vestimentas em cor verde e seu olho está totalmente branco, referenciando algum personagem de anime ou algo semelhante."></a>
+                            <figcaption class="figure-caption text-right">Fonte: Página FATECon Facebook.</figcaption>
+                        </figure>
+                        </div>
+                        <p>O desfile de cosplay foi um concurso realizado na FATECon 2023 que atraiu participantes de São Roque 
+                            e região, os participantes se fantasiaram de personagens de Anime, Séries, Jogos, Quadrinhos, Filmes
+                             e outros. O evento foi bem criativo e colorido e os participantes deram vida aos seus heróis e vilões favoritos.
+                        </p>
+                        <div class="row col-12">
+
+                        <figure class="figure col-12 imgleft figuremargin"> 
+                        <h3 class="textoimg">Primeiro Colocado do Desfile</h3>              
+                            <a href="#"><img class="figure-img img-fluid imgcem" src="imagem/fateconconccosplay/fateconc2figura2.webp" alt="Primeiro Colocado do Desfile, na imagem é mostrado o primeiro colocado do concurso vestido como Pyramid Head de Silent Hill, o personagem dele é composto por um sobretudo com sangue seguido de uma cabeça em formato de pirâmide e uma espada grande em sua mão. É possível notar o nome do cosplayer sendo ele: Bruno Siberi"></a>
+                            <figcaption class="figure-caption text-right">Fonte: Página FATECon Facebook.</figcaption>
+                        </figure>
+
+                        <figure class="figure col-6 imgleft figuremargin">
+                        <h3 class="textoimg">Segundo Colocado do Desfile</h3>               
+                            <a href="#"><img class="figure-img img-fluid imgcem" src="imagem/fateconconccosplay/fateconc2figura3.webp" alt="Segundo Colocado do Desfile, na imagem está o segundo colocado do concurso vestido de Chainsaw Man do Mangá Chainsaw Man, ele esta vestido com uma roupa estilo social e possui uma cabeça de serra elétrica junto com braços de serra elétrica. É possível notar o nome do cosplayer sendo ele: Miguel Dutra"></a>
+                            <figcaption class="figure-caption text-right">Fonte: Página FATECon Facebook.</figcaption>
+                        </figure>
+
+                        <figure class="figure col-6 imgleft figuremargin"> 
+                        <h3 class="textoimg">Terceira Colocada do Desfile</h3>              
+                            <a href="#"><img class="figure-img img-fluid imgcem" src="imagem/fateconconccosplay/fateconc2figura4.webp" alt="Terceira Colocada do Desfile, a terceira coloca do desfile está vestido de Evelyn de League of Legends, na sua vestimenta é possível notar o Cabelo esbranquecido junto de duas caldas iguais de uma raposa em sua cintura seguido de vestimentas pretas pelo corpo. É possível notar o nome da cosplayer sendo ela: Lauany M."></a>
+                            <figcaption class="figure-caption text-right">Fonte: Página FATECon Facebook.</figcaption>
+                        </figure>
+
+                        </div>
+                        
+                    </div>
+                </section>
+
+                
+            </div>
+            <a aria-hidden="true" href="#" class="bt"></a>
+        </main>
+
+<?php require "footer.inc.php"; ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
