967c01da39ce9b8feb8a5df40c433c28abbfc566
Mateus Oliveira <179388557+mateusol9812@users.noreply.github.com>
2024-11-01 19:10:55 -0300

    [Trabalho 07] Adicionando arquivos do trabalho 07

---
+++ trabalhos/atividade_07/dn.nav.inc.php
@@ -0,0 +1,26 @@
+<div class="container-fluid">
+    <div class="row align-items-end">
+        <div class="col-12">
+            <header>
+                <nav class="navbar navbar-expand-lg navbar-expand-md bg-inverse fixed-top margemheader azulescuro d-none d-md-block d-lg-block">
+                    <div class="collapse navbar-collapse justify-content-center negrito" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                              <a class="nav-link active bordapreta link-light" id="dncosplay" href="dncosplay.php #dncosplay" aria-label="Pagina de cosplays do Domingo Nerd">Cosplay</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link active bordapreta link-light" id="dnconcurso" href="dnconcursos.php #dnconcurso" aria-label="Pagina dos concuros do Domingo Nerd">Concursos</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link active bordapreta link-light" id="dnjogos" href="dnjogos.php #dnjogos" aria-label="Pagina dos jogos do Domingo Nerd">Jogos</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link active bordapreta link-light" id="dnexposicoes" href="dnexposicoes.php #dnexposicoes" aria-label="Pagina de exposições do Domingo Nerd">Exposições</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </div>     
+    </div>
+</div>
