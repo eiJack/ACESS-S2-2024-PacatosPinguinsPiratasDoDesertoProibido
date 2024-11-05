11cc8b7172b71e5df644381b4855f1d05ae42194
Gabpires <149125298+Gabpires@users.noreply.github.com>
2024-11-05 11:03:11 -0300

    [Trabalho 08] Páginas da CCXP
    adicionando o restante das páginas da CCXP

---
+++ trabalhos/atividade_08/entrevista_CCXP.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Entrevistas CCXP</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/estiloccxp.css">
+    <link rel="stylesheet" href="css/topo.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+	
+    <?php require "header.inc.php"; ?>
+    <?php require "ccxp.nav.inc.php"; ?>
+<div class="container">
+
+<article class="margementevista">
+    <div class="titulo">
+        <h2>Entrevistas</h2>        
+    </div>
+
+</article>
+
+<aside>
+    <div class= "row">      
+        <div class="col-lg-4 col-md-6 col-sm-12 ">
+            <div class="blocoentrevista">
+                <img src="imagem/jason-mamoa1.webp" alt= "Entrevista" class="img-fluid" width="300" height="200">
+                <p><b>Jason Momoa</b> é recebido ao som de gritos de 'lindo, tesão, bonito e gostosão' na CCXP.</p>
+                <p><a href="entrevista_Momoa_CCXP.php" class="btn btn-primary">Leia mais...</a></p>
+
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="blocoentrevista">
+                <img src="imagem/duna-zendaya1.webp" alt="Entrevista"  width="300" height="200">
+                <p>Entrevista com <b>Zendaya</b> e elenco de Duna faz ferver a CCXP23!</p>
+                <p><a href="entrevista_Zendaya_CCXP.php" class="btn btn-primary">Leia mais...</a></p>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="blocoentrevista">
+                <img src="imagem/compadecida1.webp" alt="Entrevista" class="img-fluid"  width="300" height="200">
+                <p><b>O Auto da Compadecida 2:</b> Elenco Ovacionado rouba a cena na CCXP23.</p>
+                <p><a href="entrevista_Compadecida_CCXP.php" class="btn btn-primary">Leia mais...</a></p>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="blocoentrevista">
+                <img src="imagem/entrevistajennaccxp.webp" alt="Entrevista" class="img-fluid">
+                <p><b>Jenna Ortega</b> E Gwendoline Christie levam fãs à loucura na CCXP22</p>
+                <p><a href="entrevista_Jenna_CCXP.php" class="btn btn-primary">Leia mais...</a></p>
+            </div>
+        </div>
+    
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="blocoentrevista">
+                <img src="imagem/entrevista2bella.webp" alt="Entrevista" class="img-fluid">
+                <p>The Last of Us: <b>Bella Ramsay</b> diz o que a aproximou de Ellie</p>
+                <p><a href="entrevista_Bella_CCXP.php" class="btn btn-primary">Leia mais...</a></p>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="blocoentrevista">
+                <img src="imagem/keanureevesccxp.webp" alt="Entrevista" class="img-fluid">
+                <p><b>Keanu Reeves</b> se ajoelha diante dos brasileiros na CCXP: 'De tirar o fôlego'.</p>
+                <p><a href="entrevista_Keanu_CCXP.php" class="btn btn-primary">Leia mais...</a></p>
+            </div>
+        </div>
+
+    </div>
+</aside>
+<a href="#" class="bt"></a>
+    </div>
+
+
+    <?php require "footer.inc.php"; ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ trabalhos/atividade_08/entrevista_Keanu_CCXP.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Entrevista com Keanu Reeves</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/estiloccxp.css">
+    <link rel="stylesheet" href="css/topo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <?php require "header.inc.php"; ?>
+    <?php require "ccxp.nav.inc.php"; ?>
+
+    <div class="container">
+
+        <article class="margementevista">
+            <div class="titulo">
+                <h3>Keanu Reeves leva público ao delírio na CCXP22</h3>
+                <p>Keanu Reeves antecipou detalhes do filme 'John Wick 4' na CCXP, em São Paulo</p>
+            </div>
+
+        </article>
+
+        <aside>
+            <div class="row">
+
+                <div class="col-lg-12 col-md-12 col-sm-6">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6">
+                            <img src="imagem/keanu2ccxp.webp" alt="Imagem entrevista" class="img-fluid" width="450"
+                                height="350">
+                        </div>
+                        <div class="col-lg-6 col-md-6">
+                            <p><b>Entrevista com Keanu Reeves</b></p>
+                            <hr>
+                            <a href="https://www.youtube.com/watch?v=eL79m43HBGE" target="_blank"
+                                class="btn btn-primary">Assista a entrevista completa</a>
+                            <p>Keanu Reeves foi um convidado especial na CCXP de 2022, em São Paulo, onde comentou sobre
+                                o filme “John Wick 4” no palco omelete, garantindo bastante ação que nunca. Momentos
+                                depois Keanu se ajoelhou diante do público brasileiro, como forma de agradecimento pelo
+                                carinho e admiração dos fãs brasileiros, levando todos a loucura no espaço.</p>
+                            <p>Durante os questionamentos feitos ao ator, foi abordado as cenas de ações consideradas
+                                “exageradas” comparado a outros títulos: "O que é exagerado? Tem mais ação neste filme.
+                                E... É, acho que o diretor tem uma visão para tentar tornar as cenas interessantes. Tem
+                                uma grande briga no meio da rua, em frente ao Arco do Triunfo, voltamos aos cavalos, tem
+                                um pouco de direção automobilística, espadas, arco e flecha... Coisas malucas de John
+                                Wick", disse Keanu.</p>
+
+
+                        </div>
+                    </div>
+                </div>
+                <p>Ao chegar no evento, ele foi recepcionado pelo público gritando: "você é de tirar o fôlego",
+                    respondendo em seguida: "O quê? Vocês que são de tirar o fôlego!".
+                </p>
+
+                <div class="row">
+
+                    <div class="col-lg-12 col-md-12 col-sm-6">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6">
+                                <img src="imagem/ccxpkeanu.webp" alt="Imagem entrevista" class="img-fluid" width="450"
+                                    height="350">
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                <p>Ao comentar sobre como é interpretar o John Wick, Keanu diz ser um papel
+                                    extraordinário, onde consegue fazer várias coisas malucas como andar a cavalo ou ter
+                                    uma briga de facas, dando ênfase ao mundo de John Wick.</p>
+                                <p>Lançado em março de 2023, o filme John Wick 4 está disponível na plataforma de
+                                    streaming Amazon Prime Vídeo onde mostra o assassino profissional que retornou às
+                                    telas para John Wick 4: Baba Yaga. O assassino profissional John Wick (Keanu Reeves)
+                                    agora virou metade do submundo contra ele com sua vingança, que agora está entrando
+                                    em sua quarta rodada em Nova York, Berlim, Paris e Osaka.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p>Fonte:
+                    https://www.uol.com.br/splash/noticias/2022/12/04/keanu-reeves-ccxp.htm
+                </p>
+            </div>
+            <a href="#" class="bt"></a>
+        </aside>
+    </div>
+
+
+
+    <?php require "footer.inc.php"; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
